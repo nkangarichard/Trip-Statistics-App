1f107d2a74b75f9d8976964981ed189942072853
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2023-07-13 10:42:14 -0400

    Initial Commit

---
+++ Nkanga_Richard_8893021_lab7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nkanga_Richard_8893021_lab7
+//
+//  Created by Richard Nkanga on 13/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
